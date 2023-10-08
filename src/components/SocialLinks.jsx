42f03a1476cf2696c851c22d916a4d5fd4322166
Shubhodeep <banerjeeshubho98@gmail.com>
2023-10-08 13:47:10 +0530

    changes

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
             Resume <BsFillPersonLinesFill size={25} />
           </>
         ),
-        href: "/Shubhodeep_Resume.pdf",
+        href: "/Shubhodeep resume.pdf",
         style: "rounded-br-md",
         download: true,
       },
